32a10608fa946a57ad16de2da5177832b7004f8c
rohan-mostofa <rohanmostofa599@gmail.com>
2023-11-02 20:33:58 +0600

    Setup redux toolkit

---
+++ front-end/src/features/auth/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        user: {},
+
+    },
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+        
+    }
+});
+
+
+// selectors
+
+// actions
+
+
+export default authSlice.reducer;
