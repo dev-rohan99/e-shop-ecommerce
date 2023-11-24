aff23fad4038e8afe28c186a6dbfef12f2945a4f
dev-rohan99 <rohanmostofa599@gmail.com>
2023-11-24 21:18:53 +0600

    Downloads page create and design

---
+++ frontend-shop/src/pages/downloads/Downloads.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Downloads = () => {
+
+
+
+    return (
+        <>
+            
+
+            
+            <div className="tab-pane" id="account-downloads">
+                <div className="icon-box icon-box-side icon-box-light">
+                    <span className="icon-box-icon icon-downloads mr-2">
+                        <i className="w-icon-download"></i>
+                    </span>
+                    <div className="icon-box-content">
+                        <h4 className="icon-box-title ls-normal">Downloads</h4>
+                    </div>
+                </div>
+                <p className="mb-4">No downloads available yet.</p>
+                <a href="shop-banner-sidebar.html" className="btn btn-dark btn-rounded btn-icon-right">Go
+                    Shop<i className="w-icon-long-arrow-right"></i></a>
+            </div>
+
+
+
+        </>
+    )
+}
+
+export default Downloads;
