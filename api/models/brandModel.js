122113774e912e7323592aad36464ed22aec9107
dev-rohan99 <rohanmostofa599@gmail.com>
2023-11-26 12:04:01 +0600

    create brand model

---
+++ api/models/brandModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+
+const brandDataSchema = mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    
+    slug: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    
+    logo: {
+        type: String,
+        required: true
+    },
+
+    status : {
+        type: Boolean,
+        default: false
+    },
+
+    trash : {
+        type: Boolean,
+        default: false
+    }
+
+}, {
+    timestamps : true
+});
+
+
+
+const brandModel = mongoose.model('brands', brandDataSchema);
+
+export default roleModel;
+
+
